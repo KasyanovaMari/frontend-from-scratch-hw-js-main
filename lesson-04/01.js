8d7d2628b4f8331d76bbbb8da04efbea9de35bf7
KasyanovaMari <magivara0@gmail.com>
2024-08-13 14:49:21 +0600

    redonel4ex1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -16,9 +16,10 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 function includesElement(array, element) {
     for(let i = 0; i < array.length; i++){
         if (array[i] === element){
-            return "true"
+            return true
         } else {
-            return "false"
+            return false
         }
     }
 }
+alert(includesElement([,1,2,3], 4))
