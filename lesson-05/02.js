2a1f351a92c208772358b2e8c9cf10fdf92860c1
KasyanovaMari <magivara0@gmail.com>
2024-08-20 17:02:29 +0600

    l5ex2done

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,10 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, artName, newValue) {
+  gallery[artName] = newValue;
+}
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+
+console.log(gallery)
