e5eb2ba2b21edf992b30bbc6a9d92523f6897fd1
KasyanovaMari <magivara0@gmail.com>
2024-09-04 01:31:42 +0600

    l7update

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,14 +6,22 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
+// const capitalizeWords = (sentence) => {
+  // let str = sentence.split(" ");
+  // //В первой строке функции `sentence.split(" ")` разбивает входную строку на массив слов. Разделителем служит пробел.
+  // for (let i = 0; i < str.length; i++) {
+  //   // Внутри цикла первое буква каждого слова `str[i][0].toUpperCase()` преобразуется в заглавную, и к ней добавляется оставшаяся часть слова `str[i].substring(1)`, начиная с индекса 1.
+  //   str[i] = str[i][0].toUpperCase() + str[i].substring(1);
+  //   }
+  //   return str.join(" "); //После завершения цикла массив слов `str` соединяется обратно в строку с помощью `str.join(" ")` и возвращается как результат функции.
+  // }
+  // console.log(capitalizeWords("hello world from javascript"));
+
 const capitalizeWords = (sentence) => {
   let str = sentence.split(" ");
-  //В первой строке функции `sentence.split(" ")` разбивает входную строку на массив слов. Разделителем служит пробел.
-  for (let i = 0; i < str.length; i++) {
-    // Внутри цикла первое буква каждого слова `str[i][0].toUpperCase()` преобразуется в заглавную, и к ней добавляется оставшаяся часть слова `str[i].substring(1)`, начиная с индекса 1.
-    str[i] = str[i][0].toUpperCase() + str[i].substring(1);
-    }
-    return str.join(" "); //После завершения цикла массив слов `str` соединяется обратно в строку с помощью `str.join(" ")` и возвращается как результат функции.
-  }
-  console.log(capitalizeWords("hello world from javascript"));
+  str = str.map(word => word[0].toUpperCase() + word.substring(1));
+  return str.join(" ");
+}
+
+console.log(capitalizeWords("hello world from javascript"));
 
--- lesson-07/02.js
+++ lesson-07/02.js
@@ -3,14 +3,11 @@
  * Если строка является числом, функция должна возвращать true, в противном случае - false.
  */
 
-function isNumeric(str) {
-  return typeof str === 'string' && str.trim() !== '' && !isNaN(Number(str))
+function isNumeric(value) {
+  return +str ? true : false
 }
- // Проверяем, является ли str строкой
-  // `typeof str === 'string'` возвращает true, если str - строка.
-  // `str.trim() !== ''` убирает пробелы в начале и конце строки и проверяет, что строка не пустая.
-  // `!isNaN(Number(str))` проверяет, можно ли преобразовать строку в число.
-  
+
+
 console.log(isNumeric("123")) // Ожидаемый результат: true
 console.log(isNumeric("12.3")) // Ожидаемый результат: true
 console.log(isNumeric("123abc")) // Ожидаемый результат: false
