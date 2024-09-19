b371b7fb92f31c017c637f7f22444538d41c13bf
KasyanovaMari <magivara0@gmail.com>
2024-09-19 21:30:29 +0600

    l9ex2done

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,21 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+  const newArr = []
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index]
+      if (callback(element, index)){
+        newArr.push(element)
+      }
+  }
+  return newArr
+  
+}
+const numbers = [1, 2, 3, 4, 5]
+
+const doubledNumbers = map(numbers, (element, index) => {
+  return element * 2
+})
+
+console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
